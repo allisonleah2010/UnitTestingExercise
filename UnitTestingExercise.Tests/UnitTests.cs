c0f3a3b53b8b306b757cf8823745fabb6b2568a8
Allison Cole <allisonleahcole@gmail.com>
2020-07-14 15:27:28 -0400

    updated

--- UnitTestingExercise.Tests/UnitTests.cs
+++ UnitTestingExercise.Tests/UnitTests.cs
@@ -6,72 +6,82 @@ namespace UnitTestingExercise.Tests
     public class UnitTests
     {
         [Theory]
-        [InlineData()]
+        [InlineData(4, 5, 6, 15)]
+        [InlineData(300,300,200,800)]
         public void Add(int num1, int num2, int num3, int expected)
         {
             //Arrange
+            var calculator = new UnitTestMethods();
 
             //Act
+            var actual = calculator.Add(num1, num2, num3);
 
             //Assert
-
+            Assert.Equal(expected, actual);
         }
 
         [Theory]
-        [InlineData()]
+        [InlineData(50, 20, 30)]
         public void Subtract(int minuend, int subtrhend, int expected)
         {
             //Arrange
+            var calculator = new UnitTestMethods();
 
             //Act
+            var actual = calculator.Subtract(minuend, subtrhend);
 
             //Assert
-
+            Assert.Equal(expected, actual);
         }
 
         [Theory]
-        [InlineData()]
+        [InlineData(10, 10, 100)]
         public void Multiply(int num1, int num2, int expected)
         {
             //Arrange
+            var calculator = new UnitTestMethods();
 
             //Act
+            var actual = calculator.Multiply(num1, num2);
 
             //Assert
-
+            Assert.Equal(expected, actual);
         }
 
         [Theory]
-        [InlineData()]
+        [InlineData(100, 2, 50)]
         public void Divide(int num1, int num2, int expected)
         {
             //Arrange
+            var calculator = new UnitTestMethods();
 
             //Act
+            var actual = calculator.Divide(num1, num2);
 
             //Assert
+            Assert.Equal(expected, actual);
 
         }
 
-        [Fact]
-        public void YourMethodName()
-        {
-            //Arrange
+        //[Fact]
+        //public void YourMethodName()
+        //{
+        //    //Arrange
 
-            //Act
+        //    //Act
 
-            //Assert
+        //    //Assert
 
-        }
+        //}
 
-        [Fact]
-        public void YourMethodName2()
-        {
-            //Arrange
+        //[Fact]
+        //public void YourMethodName2()
+        //{
+        //    //Arrange
 
-            //Act
+        //    //Act
 
-            //Assert
+        //    //Assert
         }
     }
 }
--- UnitTestingExercise/UnitTestMethods.cs
+++ UnitTestingExercise/UnitTestMethods.cs
@@ -3,15 +3,27 @@ namespace UnitTestingExercise
 {
     public class UnitTestMethods
     {
-        // Make sure your references are set properly
-            // Righ-click on Dependencies in your .Tests project and set your reference
+        // **DONE** Make sure your references are set properly
+        // **DONE** Righ-click on Dependencies in your .Tests project and set your reference
 
 
 
         // Create an Add method that passes 3 integers
 
+        public int Add(int num1, int num2, int num3)
+        {
+            return num1 + num2 + num3;
+        }
+
         // Create a Subtract method that passes 2 integers
-            // Keep track of which number is getting passed as minuend and subtrahend
+
+        public int Subtract(int minuend, int subtrhend)
+        {
+            return minuend - subtrhend;
+        }
+
+      
+        // Keep track of which number is getting passed as minuend and subtrahend
 
         // Create a Multiply method that passes 2 integers
 
@@ -21,6 +33,6 @@ namespace UnitTestingExercise
 
         // Create 2 methods that will utilize the [Fact] tests you wrote
 
-        
+
     }
 }
